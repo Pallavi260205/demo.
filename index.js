add68fc0d28a328f18075f4e86fbe1e9c2932dda
Pallavi260205 <pallavipallurao02@gmail.com>
2024-11-28 21:01:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+function home(){
+    document.getElementById("homes").style.color=" rgb(20, 251, 20)";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+
+}    
+function program(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color=" rgb(20, 251, 20)";
+
+}    
+function plan(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color=" rgb(20, 251, 20)";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+
+}    
+function blog(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color=" rgb(20, 251, 20)";
+    document.getElementById("programs").style.color="white";
+
+
+}   
+function contact(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color=" rgb(20, 251, 20)";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+
+}     
+ 
+// submit
+
+function submit(){
+    let name=document.getElementById("name:");
+    let number=document.getElementById("number");
+    let url="https://wa.me/9876543210?text=";
+    window.open(url).focus();
+}
